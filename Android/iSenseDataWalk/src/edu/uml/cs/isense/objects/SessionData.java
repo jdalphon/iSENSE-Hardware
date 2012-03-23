bd9347c56dde19f9c5398129929fe8cb2d73b28d
Jeremy Poulin <jaypoulz@gmail.com>
2012-03-23 15:11:32 -0400

    Prep to sync laptop

--- Android/iSenseDataWalk/src/edu/uml/cs/isense/objects/SessionData.java
+++ Android/iSenseDataWalk/src/edu/uml/cs/isense/objects/SessionData.java
@@ -1,9 +1,5 @@
 package edu.uml.cs.isense.objects;
 
-import java.util.ArrayList;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 public class SessionData {
 	public JSONObject RawJSON = null;
